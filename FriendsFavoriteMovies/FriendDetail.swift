7a864737ed2ca036e0dc75227e10fdaa412564bb
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-04-07 02:25:18 +0300

    Initial commit

---
+++ FriendsFavoriteMovies/FriendDetail.swift
@@ -0,0 +1,18 @@
+//
+//  FriendDetail.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Victoria Vavulina on 6.4.2026.
+//
+
+import SwiftUI
+
+struct FriendDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendDetail()
+}
---
+++ FriendsFavoriteMovies/MovieDetail.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetail.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Victoria Vavulina on 6.4.2026.
+//
+
+import SwiftUI
+
+struct MovieDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetail()
+}
