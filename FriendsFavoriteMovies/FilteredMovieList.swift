66487c02f6d25b61e539ed435c8acc28a7ec9aa0
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2026-04-07 04:27:18 +0300

    Initial commit

---
+++ FriendsFavoriteMovies/FilteredMovieList.swift
@@ -0,0 +1,18 @@
+//
+//  FilteredMovieList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Victoria Vavulina on 7.4.2026.
+//
+
+import SwiftUI
+
+struct FilteredMovieList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilteredMovieList()
+}
